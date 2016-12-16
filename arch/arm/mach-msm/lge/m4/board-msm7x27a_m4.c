5ab35af4c416ec2ba296c7f5b0a3b8b102da199d
JonasCardoso <jonascard60@gmail.com>
2016-12-16 13:54:43 -0200

    cyanogenmod_u0_nonfc_defconfig

--- arch/arm/mach-msm/lge/m4/board-msm7x27a_m4.c
+++ arch/arm/mach-msm/lge/m4/board-msm7x27a_m4.c
@@ -276,10 +276,10 @@ static struct msm_i2c_platform_data msm_gsbi1_qup_i2c_pdata = {
 
 #ifdef CONFIG_ARCH_MSM7X27A
 /* JonasCardoso */
-#define MSM_RESERVE_MDP_SIZE       	    0x1600000 	// 0x2300000 
-#define MSM7x25A_MSM_RESERVE_MDP_SIZE       0x1600000
+#define MSM_RESERVE_MDP_SIZE       	    0x1550000 	// 0x2300000 
+#define MSM7x25A_MSM_RESERVE_MDP_SIZE       0x1550000
 
-#define MSM_RESERVE_ADSP_SIZE      	    0x1500000
+#define MSM_RESERVE_ADSP_SIZE      	    0x1600000
 #define MSM7x25A_MSM_RESERVE_ADSP_SIZE      0xB91000
 #define CAMERA_ZSL_SIZE		(SZ_1M * 60)
 #endif
